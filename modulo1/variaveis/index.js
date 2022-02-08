647d4a347ea9cedcb8d8655c953995f33307dd22
bruno-macena <bdsm4@hotmail.com>
2022-02-08 01:46:05 -0300

    Criei um arquivo para treinamento em Javascript

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,63 @@
+// let a = 10
+// let b = 10
+// console.log(b)
+// b = 5
+// console.log(a,b)
+//10 e 10,5
+//------------
+// let a = 10
+// let b = 20
+// c = a
+// b = c
+// a = b
+// console.log(a,b,c)
+// //10,10,10
+//------------
+// let horasTrabalhadas = prompt("Quantas horas  você trabalha por dia ?")
+// let ganhosPorDia = prompt("Quanto você recebe por dia ?")
+// alert ('Voce recebe ${horasTrabalhadas/ganhosPorDia} por hora')  // sem resultados, o programa não executa o cálculo
+//------------
+// const seuNome = prompt("Qual o seu nome ?")
+// const suaIdade = Number (prompt("Qual a sua idade ?"))
+// //console.log(typeof seuNome)
+// //console.log(typeof suaIdade)
+// //sting, number
+// console.log("Olá",seuNome, "Voce tem",suaIdade,"anos")
+//------------
+// const meteorologia = prompt("Choveu hoje na sua cidade ?")
+// console.log(meteorologia)
+
+// const curso = prompt("Está gostando do curso da Labenu ?")
+// console.log(curso)
+
+// const metodologia = prompt("A metodologia do curso está bom para todos ?")
+// console.log(metodologia)
+//------------
+
+// const a = "sim"
+// const b = "nao" 
+// const c = "talvez" 
+
+// const meteorologia = ("Choveu hoje na sua cidade ?")
+// console.log(meteorologia,a)
+
+// const curso = ("Está gostando do curso da Labenu ?")
+// console.log(curso,a)
+
+// const metodologia = ("A metodologia do curso está bom para todos ?")
+// console.log(metodologia,c)
+//------------
+// let a = 10
+// let b = 25
+// c = a
+// a = b 
+// b = c
+
+// console.log("O novo valor de a é",a, "O novo valor de b é",b,)
+// //a = 25 e b = 10
+//------------
+// let numero1 = prompt("Insira um valor")
+// let numero2 = prompt("Insira um valor")
+// console.log("O primeiro número somado ao segundo número resulta em:",numero1+numero2)
+// console.log("O primeiro número multiplicado pelo segundo número resulta em:",numero1*numero2)
+// // //A Soma não é realizada, somente a únião dos dois valores e a multiplicação é compreendida e executada corretamente.
