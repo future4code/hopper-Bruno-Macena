06c57bbca5d301025eefe0b1b10804dc9ad5f973
bruno-macena <bdsm4@hotmail.com>
2022-02-15 00:32:15 -0300

    Exercicio de segunda feira

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,44 @@
+// let array
+// console.log('a. ', array)
+
+// array = null
+// console.log('b. ', array)
+
+// array = [3,4,5,6,7,8,9,10,11,12,13]
+// console.log('c. ', array.length)
+
+// let i = 0
+// console.log('d. ', array[i])
+
+// array [i+1] = 19
+// console.log('e. ',array)
+
+// const valor = array[i+6]
+// console.log('f. ',valor)
+
+// A: Undefined B: Null C:11 D:3 E: [3,19,5..] F:9
+
+// const frase = prompt("Digite uma frase")
+// console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+//Irá alterar todas as letras minúsculas em maiúsculas e substituir todas
+// as letras "A" pela letra "I" e mostrar no sistema quantos caracteres possui
+// a frase, incluindo os espaços
+// SUBI NUM ÔNIBUS EM MIRROCOS 27
+
+// Exercício 1
+
+//const cadastroUser = prompt("Qual o seu nome de usuário ?")
+//const cadastroEmail = prompt("Qual é o seu e-mail ?")
+//console.log("O e-mail " +cadastroEmail+ "foi cadastrado com sucesso. Seja bem vindo(a)," +cadastroUser)
+//console.log(`O email ${cadastroEmail} foi cadastrado com sucesso. Seja bem vindo(a)${cadastroUser}`)
+
+// Exercício 2
+
+// const comidasFavoritas = ["Lasanha", "Strogonoff", "Pizza", "Espinafre", "Macarronada"]
+// // console.log(comidasFavoritas)
+// // console.log("Essas são as minhas comidas preferidas" ,)
+// const favoriteFood = prompt("Qual o seu prato predileto ?")
+// favoriteFood = comidasFavoritas.splice(2, 1)
+// console.log(favoriteFood)
+
+
