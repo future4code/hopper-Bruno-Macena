83e8977cdb1de6192d64cd74af7466ae834d8cc5
bruno-macena <bdsm4@hotmail.com>
2022-02-22 23:37:33 -0300

    Exercicios de objetos 22/02

--- modulo1/objetos/index.js
+++ modulo1/objetos/index.js
@@ -33,21 +33,58 @@
 
  //Exercício 2
  //A-
-const aluno1= {
-nome: "Bruno",
-idade: 32,
-profissao: "Backoffice"
-}
 
-const aluno2={
-...aluno1,
-nome:"Andre",
-idade:20,
-profissao: "Youtuber"
-}
-//B-
+ 
+// const aluno1= {
+// nome: "Bruno",
+// idade: 32,
+// profissao: "Backoffice"
+//  }
+// const aluno2={
+// ...aluno1,
+// nome:"Andre",
+// idade:20,
+// profissao: "Youtuber"
+// }
+// //B-
+// const dados = (cadastro)=>{
+// return [cadastro.nome, cadastro.nome.length, 
+//     cadastro.idade, cadastro.profissao, 
+//     cadastro.profissao.length]
 
-const cadastro1 = (aluno1) => {
-console.log(aluno1.nome.length)
-}
-console.log(cadastro1)
+// }
+// console.log(dados(aluno1))
+// console.log(dados(aluno2))
+
+
+//Exercício 3
+// const carrinho = []
+// const frutas1 = {nome:"Maca", disponibilidade:true}
+// const fruta2 = {nome:"Melancia", disponibilidade:true}
+// const fruta3 = {nome: "Jaca", disponibilidade:true}
+// const sacola = (mercado1, mercado2, mercado3)=>{
+// carrinho.push(mercado1, mercado2, mercado3)
+// return [mercado1, mercado2, mercado3]
+// }
+
+// console.log(sacola(frutas1,fruta2,fruta3))
+
+//Desafio 1
+// const dados1 = []
+// const nome = "Bruno"
+// const idade = 32
+// const profissao = "Backoffice"
+// console.log(nome, idade, profissao)
+// const dadosUsuario = [nome, idade, profissao]
+// const dados2 = (cadastro)=>{
+// dados1.push(cadastro)
+// }
+// dados2(dadosUsuario)
+// console.log(dados1)
+
+
+
+
+
+
+
