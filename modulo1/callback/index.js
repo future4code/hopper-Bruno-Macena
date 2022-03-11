60b80ecd2d49cc278c168b1256df0d364c9a3b22
bruno-macena <bdsm4@hotmail.com>
2022-03-10 23:03:27 -0300

    Entrega de Exercício

--- modulo1/callback/index.js
+++ modulo1/callback/index.js
@@ -19,49 +19,85 @@ const pets = [
     { nome: "Caramelo", raca: "Vira-lata"},
 ]
 //A-
-// const novoArrayA = pets.map((item, index) => {
-//     console.log(item.nome)
-// })
+const novoArrayA = pets.map((item) => {
+    console.log(item.nome)
+})
 //B-
-// const novoArrayB = pets.filter((item, index) => {
-//     return item.raca === "Salsicha"
-//  })
-//   console.log(novoArrayB)
+const novoArrayB = pets.filter((item) => {
+    return item.raca === "Salsicha"
+ })
+  console.log(novoArrayB)
 
 //C- 
-const novoArrayC = pets.filter((item, index) => {
-    return(item.raca === "Poodle")
+const novoArrayC = (item) => {
+    return item.raca === "Poodle"
     
+}
+
+const cachorrosSelecao = pets.filter(novoArrayC)
+
+const cupomTosa = (item) => {
+    return item.nome
+}
+const premioTosa = cachorrosSelecao.map(cupomTosa)
+
+console.log("Você ganhou um cupom de desconto de 10% para tosar o/a " ,premioTosa[0])
+console.log("Você ganhou um cupom de desconto de 10% para tosar o/a " ,premioTosa[1])
+
+//2-
+
+const produtos = [
+    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+    { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+    { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+]
+//A-
+const novoArrayProdutosA = produtos.map((item) => {
+    console.log(item.nome)
 })
+//B-
+const novoArrayProdutosB = produtos.map((item) => {
+    return {nome:item.nome,preco:item.preco-item.preco * 0.05}
+ })
 
-const cumpomTosa = pets.map((item,index) => {
-    console.log(`Você ganhou um cupom de desconto de 10% para tosar o/a ${novoArrayC}!`)
-    
+  console.log(novoArrayProdutosB)
+//C-
+const novoArrayProdutosC = produtos.filter((item) => {
+    return item.categoria === "Bebidas"
 })
+console.log(novoArrayProdutosC)
+//D-
+const novoArrayProdutosD = produtos.filter((item) => {
+    return item.nome.includes('Ypê')
+})
+console.log(novoArrayProdutosD)
+//E-
+const novoArrayProdutosE = produtos.filter((item => {
+    return item.nome.includes("Ypê")
+}))
+const compraYpe = novoArrayProdutosE.map((item) => {
+    return `Compre ${item.nome} por ${item.preco}!`
+})
+console.log(compraYpe)
 
+//Desafio 1
 
-// const recebePoodle = (novoArrayC)
-// console.log(`Você ganhou um cupom de desconto de 10% para tosar o/a ${novoArrayC}!`)
-// novoArrayC(`Você ganhou um cupom de desconto de 10% para tosar o/a ${}!`)
-
-// console.log(novoArrayC.)
-// console.log(`Você ganhou um cupom de desconto de 10% para tosar o/a !`(novoArrayC))
-
-
-
-// const verificaImpar = (numero, imprimir)=>{
-//     if(numero % 2 !== 0){
-//         imprimir()
-//     }
-// }
-
-// const imprimirImpar = ()=>{
-//     console.log(`Sim é impar`)
-// }
-// verificaImpar(25, imprimirImpar)
-
-// const arrayDeNumeros = [10,20,30,40,50]
-
-// const imprimirNumeros = arrayDeNumeros.map((numero, indice)=>{
-//     console.log(`O número é ${numero} e sua posição é ${indice}`)
-// })
+const pokemons = [
+    { nome: "Bulbasaur", tipo: "grama" },
+    { nome: "Bellsprout", tipo: "grama" },
+    { nome: "Charmander", tipo: "fogo" },
+    { nome: "Vulpix", tipo: "fogo" },
+    { nome: "Squirtle", tipo: "água" },
+    { nome: "Psyduck", tipo: "água" },
+ ]
+//A-
+const  desafioSort = pokemons.map((item) => item.nome).sort()
+console.log(desafioSort)
+//End...
