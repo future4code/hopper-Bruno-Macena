fcdae39945d009d96d7eabf8150bf3e9ee77f76d
bruno-macena <bdsm4@hotmail.com>
2022-03-12 20:24:39 -0300

    Correção de console

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -1,33 +1,30 @@
 // Jogo Blackjack (21) Projeto Labenu - Bruno de Salles M.
 
 console.log("Boas vindas ao jogo de Blackjack!")
-   if(confirm("Quer iniciar uma nova rodada?")) {
-   }
-   else{
-console.log("O jogo acabou")
-   }
-   const cartaUsuario = comprarCarta()
-   const cartaComputador = comprarCarta()
-   const novaCartaUsuario = comprarCarta()
-   const novaCartaComputador = comprarCarta()
-   const pontosUsuario = cartaUsuario.valor + novaCartaUsuario.valor
-   const pontosComputador = cartaComputador.valor + novaCartaComputador.valor
-console.log(`As suas cartas foram: ${cartaUsuario.texto} e ${novaCartaUsuario.texto} pontuação final: ${pontosUsuario}`)
-console.log(`As cartas do computador foram: ${cartaComputador.texto} e ${novaCartaComputador.texto} pontuação final: ${pontosComputador}`)
-   if(pontosUsuario > pontosComputador){
-console.log("O Usuário venceu!!!")
-   }else if(pontosUsuario < pontosComputador){
-console.log("O computador venceu!!!")
-   }else if(pontosUsuario === pontosComputador){
-console.log("O jogo terminou em empate")
+if (confirm("Quer iniciar uma nova rodada?")) {
+   const cartaUsuario = [comprarCarta(), comprarCarta()]
+   const cartaComputador = [comprarCarta(), comprarCarta()]
+   const pontosUsuario = [cartaUsuario[0].valor + cartaUsuario[1].valor]
+   const pontosComputador = [cartaComputador[0].valor + cartaComputador[1].valor]
+   console.log(`As suas cartas foram: ${cartaUsuario[0].texto} e ${cartaUsuario[1].texto} pontuação final: ${pontosUsuario}`)
+   console.log(`As cartas do computador foram: ${cartaComputador[0].texto} e ${cartaComputador[1].texto} pontuação final: ${pontosComputador}`)
+   if (pontosUsuario > pontosComputador) {
+      console.log("O Usuário venceu!!!")
+   } else if (pontosUsuario < pontosComputador) {
+      console.log("O computador venceu!!!")
+   } else if (pontosUsuario === pontosComputador) {
+      console.log("O jogo terminou em empate")
    }
    else {
-console.log("Não houve um vencedor")
-}   
-
+      console.log("Não houve um vencedor")
+   }
+}
+else {
+   console.log("O jogo acabou")
+}
 
 /**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'S
  * 
  * 
     const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
