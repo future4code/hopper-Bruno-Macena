709d03adff54cc416457a7a44a0571cb4706a28c
bruno-macena <bdsm4@hotmail.com>
2022-03-18 22:29:57 -0300

    Lista de exercícios 05-13

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -5,7 +5,7 @@
 
 // EXERCÍCIO 01
 function retornaTamanhoArray(array) {
-   return array.length
+    return array.length
 }
 
 // EXERCÍCIO 02
@@ -15,7 +15,7 @@ function retornaArrayInvertido(array) {
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-    return array.sort((a,b)=>a-b)
+    return array.sort((a, b) => a - b)
 }
 
 // EXERCÍCIO 04
@@ -27,13 +27,13 @@ function retornaNumerosPares(array) {
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
     let numerosPares = []
-    for(let i = 0; i < array.length; i++) {
-        if (array[i] % 2 === 0){
-            numerosPares.push((array[i]*array[i]))
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            numerosPares.push((array[i] * array[i]))
         }
     }
-   return numerosPares
-  
+    return numerosPares
+
 }
 
 
@@ -44,12 +44,12 @@ function retornaMaiorNumero(array) {
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
-const  objeto = {
-  maiorNumero:Math.max(num1, num2),
-  maiorDivisivelPorMenor: Math.max(num1, num2) % Math.min(num1, num2) === 0,
-diferenca: Math.max(num1, num2) - Math.min(num1, num2)
-}
-return objeto
+    const objeto = {
+        maiorNumero: Math.max(num1, num2),
+        maiorDivisivelPorMenor: Math.max(num1, num2) % Math.min(num1, num2) === 0,
+        diferenca: Math.max(num1, num2) - Math.min(num1, num2)
+    }
+    return objeto
 }
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
@@ -60,59 +60,80 @@ function retornaNPrimeirosPares(n) {
         }
     }
     return numerosPares;
-      
+
 }
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-// return ladoA * ladoB * ladoC
+    if (ladoA === ladoB && ladoB === ladoC) {
+        return "Equilátero"
+    } else if (ladoA === ladoB || ladoA === ladoC || ladoB === ladoC) {
+        return "Isósceles"
+    } else {
+        return "Escaleno"
+    }
 }
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
- 
+
     let comparativos = array.sort((a, b) => a - b)
-    return [comparativos[comparativos.length-2], comparativos[1]]
+    return [comparativos[comparativos.length - 2], comparativos[1]]
 }
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores.join(", ")}.`
-    
-   
+    return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores.join(", ")}.`
+
+
 }
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+    const anonimo = {
+        ...pessoa,
+        nome: 'ANÔNIMO'
+    }
+    return anonimo
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   let permissao = []
-   for(const pessoa of pessoas){
-       if(
-           pessoa.altura > 1.5 && idade > 14 && pessoa < 60){
-          permissao.push(pessoa);
-            }
+    let permissao = []
+    for (const pessoa of pessoas) {
+        if (
+            pessoa.altura > 1.5 && pessoa.idade > 14 && pessoa.idade < 60) {
+            permissao.push(pessoa);
         }
-   return permissao;
+    }
+    return permissao;
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+    let naoTemPermissao = []
+    for (const pessoa of pessoas) {
+        if (pessoa.altura < 1.5 || pessoa.idade <= 14 || pessoa.idade >= 60) {
+            naoTemPermissao.push(pessoa)
+        }
+    }
+    return naoTemPermissao
 }
 
 // EXERCÍCIO 14
 function retornaContasComSaldoAtualizado(contas) {
-
-}
+    contas.map((conta) => {
+        let gastos = conta.compras.reduce((num1, num2) => num1 + num2, 0)
+        conta.saldoTotal -= gastos
+        conta.compras = []
+    })
+    return contas
+            }
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-// return consultas.sort((a,b)=>a-b)
+    return consultas.sort((a, b) => a.nome.localeCompare(b.nome))
 }
 
 // EXERCÍCIO 15B
 function retornaArrayOrdenadoPorData(consultas) {
-   
+
 }
