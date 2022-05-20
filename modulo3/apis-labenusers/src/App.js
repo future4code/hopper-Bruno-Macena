8c8962a2774a83e98bd8fbb5922ece9d2a65d30b
bruno-macena <bdsm4@hotmail.com>
2022-05-19 23:57:05 -0300

    Entrega incompleta do exercício

---
+++ modulo3/apis-labenusers/src/App.js
@@ -0,0 +1,14 @@
+import integracaoApi from "./IntegracaoApi";
+
+function App() {
+
+  return (
+    <div div className="App">
+      <p></p>
+      <integracaoApi></integracaoApi>
+
+    </div>
+  );
+}
+
+export default App;
