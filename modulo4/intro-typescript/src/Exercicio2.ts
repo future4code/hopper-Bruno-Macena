070108515bd0d767caa13c614f37af90b5ad75c7
bruno-macena <bdsm4@hotmail.com>
2022-08-09 23:00:59 -0300

    Exercicio Intro-Typescript

---
+++ modulo4/intro-typescript/src/Exercicio2.ts
@@ -0,0 +1,10 @@
+//Exercício 2
+
+function imprimeTresCoresFavoritas():void {
+    const cor1:string = "Azul"
+    const cor2:string = "Roxo"
+    const cor3:string = "Vermelho"
+    console.log([cor1, cor2, cor3])
+    
+}
+imprimeTresCoresFavoritas()
